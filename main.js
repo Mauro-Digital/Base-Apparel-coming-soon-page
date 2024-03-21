36c0e78a5f7b0d72a0ff5420b90182832e911174
Mauro-Digital <141379319+Mauro-Digital@users.noreply.github.com>
2024-03-20 20:21:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('container__content__form');
+const imputEmail = document.querySelector('.container__content__form__email');
+const error = document.querySelector('.container__content__form__email--error');
+
+form.addEventListener('submit', (event)=>{
+    
+    console.log('presionado el boton');
+});
